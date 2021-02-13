6ab034cccdd97351993ae45ea535d7068abe938f
Gilad Ben Dov <giladbendov@gmail.com>
2021-02-13 13:00:10 +0200

    Documentation setup

--- examples/function.py
+++ examples/function.py
@@ -11,6 +11,7 @@ emu.mem.write_code(seg.address, """
     POP {PC}
 """, isa)
 
+emu.allocate_stack()
 emu.regs.set(r0=3, r1=5, r2=1)
 print(emu.run_function(seg.address, isa=isa))
 print(emu.run_function(seg.address, isa=isa))
