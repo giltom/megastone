5b73c0d9ce23e98ebe247de4b7d328250329d3af
Gilad Ben Dov <giladbendov@gmail.com>
2021-02-27 12:26:33 +0200

    Remove GenericArchitecture

--- megastone/arch/arches/__init__.py
+++ megastone/arch/arches/__init__.py
@@ -2,4 +2,3 @@ from .x86 import X86_REGS, ARCH_X86_16, ISA_X86_16, ARCH_X86, ISA_X86, ARCH_X86_
 from .arm import ARM_REGS, ARCH_ARM, ISA_ARM, ISA_THUMB
 from .arm64 import ARM64_REGS, ARCH_ARM64, ISA_ARM64
 from .mips import MIPS_REGS, ARCH_MIPS, ISA_MIPS, ARCH_MIPS64, ISA_MIPS64
-from .generic import GenericArchitecture
